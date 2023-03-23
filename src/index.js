6717d5aeae19a494354e4560afa5914620db7a40
k8pai <thek8pai@gmail.com>
2023-03-23 23:56:41 +0530

    modified index.js with exprots

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as TiNumber } from './components/TiNumber';
+export { default as TiMail } from './components/TiMail';
+export { default as TiText } from './components/TiText';
+export { default as TiFiles } from './components/TiFiles';
